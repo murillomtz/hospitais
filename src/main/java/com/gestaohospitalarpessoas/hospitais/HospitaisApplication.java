d8e1882cf1596a063cc095131db7f5552d5fa4b8
murillomtz <60979842+murillomtz@users.noreply.github.com>
2024-04-09 12:45:44 -0300

    first commit

---
+++ src/main/java/com/gestaohospitalarpessoas/hospitais/HospitaisApplication.java
@@ -0,0 +1,13 @@
+package com.gestaohospitalarpessoas.hospitais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaohospitalarpessoas/hospitais/HospitaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaohospitalarpessoas.hospitais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
